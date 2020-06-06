cd151692e009a3ee47d15d52c8d29aa3bc13e048
naojitaniguchi <naoji.taniguchi@gmail.com>
2020-06-06 11:52:59 +0900

    First commit

---
+++ Assets/Player.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float forceScale = 0.1f ;
+    public float MaxVelocity = 0.1f ;
+    private bool mouseDown;
+    private Vector3 mouseDownPos;
+
+    // Start is called before the first frame update
+
+
+    void Start()
+    {
+        mouseDown = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if ( Input.GetMouseButtonDown(0))
+        {
+            mouseDown = true;
+            mouseDownPos = Input.mousePosition;
+        }
+
+        if (Input.GetMouseButtonUp(0))
+        {
+            mouseDown = false;
+        }
+        if (mouseDown)
+        {
+            //Debug.Log(Input.mousePosition);
+            Vector3 dragVector = Input.mousePosition - mouseDownPos;
+            //Debug.Log(dragVector);
+
+            Vector2 forceVec;
+            forceVec.x = dragVector.x;
+            forceVec.y = dragVector.y;
+            forceVec *= forceScale;
+
+            gameObject.GetComponent<Rigidbody2D>().AddForce(forceVec);
+            float velocityMagitude = gameObject.GetComponent<Rigidbody2D>().velocity.magnitude;
+            if (velocityMagitude > MaxVelocity)
+            {
+                gameObject.GetComponent<Rigidbody2D>().velocity.Normalize();
+                gameObject.GetComponent<Rigidbody2D>().velocity *= MaxVelocity;
+            }
+        }
+
+
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# AstronautSample
+[![youtube](https://img.youtube.com/vi/kMCdJd73B84/0.jpg)](http://www.youtube.com/watch?v=kMCdJd73B84 "Astoronaut game sample.")
